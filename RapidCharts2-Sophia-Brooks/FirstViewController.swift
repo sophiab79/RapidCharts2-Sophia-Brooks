04afe23ac71065d8b8257eae0ab1623976d4ce2a
sophiab79 <sophiabrooks998@gmail.com>
2020-12-13 16:13:52 -0800

    Initial Commit

---
+++ RapidCharts2-Sophia-Brooks/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RapidCharts2-Sophia-Brooks
+//
+//  Created by Sophia on 12/13/20.
+//  Copyright © 2020 Sophia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RapidCharts2-Sophia-Brooks/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RapidCharts2-Sophia-Brooks
+//
+//  Created by Sophia on 12/13/20.
+//  Copyright © 2020 Sophia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
